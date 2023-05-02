a8a286e25d2a95703ab26c9c4f29517fa88d8b3a
AngelACrawford <aac72646@uga.edu>
2023-05-01 23:26:21 -0400

    Fixed Login

--- app/src/main/java/edu/uga/cs/rommateshopping/LoginActivity.java
+++ app/src/main/java/edu/uga/cs/rommateshopping/LoginActivity.java
@@ -19,62 +19,113 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity{
 
-    private Button registerButton;
-    private Button loginButton;
+    public static final String TAG = "Sign in Activity";
+
+    private FirebaseAuth mAuth;
+    private Button login;
+
+    private EditText emailText;
+    private EditText passwordText;
 
     /**
-     * Creates the main menu with login/register for the app
-     *
-     * @param savedInstanceState the bundled saved state of the application
+     * Creates the basic information for the login activity
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+        mAuth = FirebaseAuth.getInstance();
+        login = findViewById(R.id.loginButton);
+        emailText = findViewById(R.id.emailText2);
+        passwordText = findViewById(R.id.passwordText2);
 
-        registerButton = findViewById(R.id.splashRegisterButton);
-        loginButton = findViewById(R.id.splashLoginButton);
-
-        // Assigning the onClickListener for the register button
-        registerButton.setOnClickListener(new RegisterButtonClickListener());
-        // Assigning the onClickListener for the login button
-        loginButton.setOnClickListener(new LoginButtonClickListener());
+        //listener for the login button
+        login.setOnClickListener(new LoginActivity.ButtonClickListener());
     }
 
     /**
-     * This class defines the onClickListener class for the Registration button. This listener
-     * opens the RegisterActivity class.
+     * On start, gets the current user
      */
-    public class RegisterButtonClickListener implements View.OnClickListener {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    /**
+     * Listener for the login button.
+     */
+    private class ButtonClickListener implements View.OnClickListener {
+
         /**
-         * Runs when register is clicked. When it is clicked, it opens a new intent for the page
-         * that contains the register activity.
-         *
-         * @param v the button view that has called the method
+         * on click, gets the email and password from the
+         * edit views and calls the signInUser method
          */
         @Override
         public void onClick(View v) {
-            Intent intent = new Intent(v.getContext(), RegisterActivity.class);
-            v.getContext().startActivity(intent);
+            String email = emailText.getText().toString();
+            String password = passwordText.getText().toString();
+            signInUser(email, password);
         }
     }
 
     /**
-     * This class defines the onClickListener class for the Login button. This listener
-     * opens the LoginActivity class.
+     * Attempts to sign in a user based on the information
+     * they have provided to the app.
+     *
+     * @param email the email that was retrieved from the user input
+     * @param password the password retrieved from the user's input
      */
-    public class LoginButtonClickListener implements View.OnClickListener {
-        /**
-         * Runs when login is clicked. When it is clicked, it opens a new intent for the page
-         * that contains the login page.
-         *
-         * @param v the button view that has called the method
-         */
-        @Override
-        public void onClick(View v) {
-            Intent intent = new Intent(v.getContext(), LoginActivity.class);
-            v.getContext().startActivity(intent);
-        }
+    private void signInUser(String email, String password) {
+        Log.d(TAG, "signInUser:" + email);
+
+        //attempts a sign in
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    /**
+                     * Attempts a sign in. If successful sign in, display successful message
+                     * and then calls updateUI method. If unsuccessful, does not allow
+                     * the user to go on to the next screen and displays an error.
+                     *
+                     * @param task the sign-in attempt
+                     */
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                    }
+                });
     }
+
+    /**
+     * This is called on a successful sign in. When someone signs in,
+     * this method will update the UI to now go to the MainNavigationActivity and display a
+     * successful sign in message.
+     *
+     * @param account the current FirebaseUser's information
+     */
+    public void updateUI(FirebaseUser account){
+
+        if(account != null){
+            Toast.makeText(this,"Signed in successfully.",Toast.LENGTH_LONG).show();
+            startActivity(new Intent(this, MainNavigationActivity.class));
+
+        }else {
+            Toast.makeText(this,"Error signing in: Could not find this user.",Toast.LENGTH_LONG).show();
+        }
+
+    }
+
 
 }
